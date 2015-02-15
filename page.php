570a8a537202b58c7576af6127007e4a9cfb273f
corbanCloud <corban.cloud@gmail.com>
2015-02-14 21:59:06 -0800

    Testing web hook

--- page.php
+++ page.php
@@ -6,9 +6,7 @@
 
 get_header(); 
 
-if ( have_posts() ) : while ( have_posts() ) : the_post();  
-
-?>
+if ( have_posts() ) : while ( have_posts() ) : the_post();  ?>
 
 
 <?php endwhile; endif; // End WP Loop ?>
